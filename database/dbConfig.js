1daa93a7b446b55b812b5973fce5432e5f65c923
Chris Kuh <kuh_chris@yahoo.com>
2016-02-21 18:14:05 -0800

    [feature] deploy

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,9 +1,19 @@
 var mysql = require('mysql');
 
-var connection = {
-  user: "root",
-  password: "",
-  database: "gameswap"
-};
+var connection = {};
 
+if (process.env.DATABASE_URL) {
+  connection = {
+    host: "process.env.DATABASE_URL",
+    user: "root",
+    password: "",
+    database: "gameswap"
+  };
+} else {
+  connection = {
+    user: "root",
+    password: "",
+    database: "gameswap"
+  };
+}
 module.exports = connection;
