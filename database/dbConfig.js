65057a2dbd7a4c65e3ac77bd61330d4bcbfe90ca
Chris Kuh <kuh_chris@yahoo.com>
2016-02-21 19:18:51 -0800

    [fix] edit connection settings

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,13 @@ var mysql = require('mysql');
 var connection = {};
 
 if (process.env.DATABASE_URL) {
-  connection = process.env.DATABASE_URL
+  connection = {
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_DATABASE,
+    user: process.env.DATABASE_USER,
+    port: process.env.DATABASE_PORT,
+    password: process.env.DATABASE_PASSWORD
+  };
 } else {
   connection = {
     user: "root",
